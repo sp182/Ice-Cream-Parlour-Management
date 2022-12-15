01dfd6b43d84cfbe07f453aa6bf34a4702b92df5
sp182 <sanjeevani.pramanik2020@vitstudent.ac.in>
2022-12-15 22:28:28 +0530

    updated

--- idea.c
+++ idea.c
@@ -1,12 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #define max 5
-int push(char stack[max][80], int *top, char data[80]) //This function 
-pushes the item (flavour) to stack (ice-cream) 
+int push(char stack[max][80], int *top, char data[80]) //This function pushes the item (flavour) to stack (ice-cream) 
 {
  if (*top == max - 1)
- 3
  return (-1);
  else
  {
@@ -15,8 +13,7 @@ pushes the item (flavour) to stack (ice-cream)
  return (1);
  }
 }
-int pop(char stack[max][80], int *top, char data[80]) //This function 
-removes the topmost item (flavour) from the stack (ice cream) 
+int pop(char stack[max][80], int *top, char data[80]) //This function removes the topmost item (flavour) from the stack (ice cream) 
 {
  if (*top == -1)
  return (-1);
@@ -27,8 +24,7 @@ removes the topmost item (flavour) from the stack (ice cream)
  return (1);
  }
 }
-void display(char stack[max][80], int *top, char data[80]) //Displays 
-the whole stack (ice cream)
+void display(char stack[max][80], int *top, char data[80]) //Displays the whole stack (ice cream)
 {
  int i;
  for (i = *top; i >= 0; i--)
@@ -40,14 +36,13 @@ the whole stack (ice cream)
  printf("Stack is empty");
  }
 }
-int insq(char queue[max][80], int *rear, char data[80]) //Enqueues 
-the name of the customer
+int insq(char queue[max][80], int *rear, char data[80]) //Enqueues the name of the customer
 {
  if (*rear == max - 1)
  return (-1);
  else
  {
- 4
+ 
  *rear = *rear + 1;
  strcpy(queue[*rear], data);
  return (1);
@@ -76,16 +71,15 @@ int main() //Main function
  top = -1; // Initialize Stack
  front = rear = -1; //... Initialize a Queue
  printf("-------------------------------------------\n");
- printf("\t Welcome to \n\t 'Scoops n Smiles' \n\t Ice-cream 
-Parlour");
+ printf("\t Welcome to \n\t 'Scoops n Smiles' \n\t Ice-cream Parlour");
  printf("\n-------------------------------------------\n");
  printf("\n 1. Write your name to stand in a Queue");
- printf("\n 2. Delete your name from a Queue if you don't want icecream");
+ printf("\n 2. Delete your name from a Queue if you don't want icecream");
  printf("\n 3. Choose your ice cream flavour");
  printf("\n 4. Good-bye");
  printf("\n------------------------------\n");
  while (1)
- 5
+ 
  {
  printf("Choose operation : ");
  scanf("%d", &ch);
@@ -119,13 +113,12 @@ data);
  printf("\n----------------------------\n");
 printf("\tMENU CARD");
  printf("\n----------------------------\n");
- printf("\n Make your own ice-cream here by choosing the 
-below options.... :)\n");
+ printf("\n Make your own ice-cream here by choosing the below options.... :)\n");
  printf("\n");
  printf(" 1. Enter your flavour (Push)\n");
  printf(" 2. Remove your flavour (Pop)\n");
  printf(" 3. Here is your ice-cream\n");
- 6
+ 
  printf(" 4. Make your payment\n");
  printf("----------------------------\n");
  while (1)
@@ -139,33 +132,28 @@ below options.... :)\n");
  scanf("%s", nm);
  reply = push(stack, &top, nm);
  if (replyst == -1)
- printf("\n You can't add more flavours to the ice-cream 
-(Stack is Full).\n");
+ printf("\n You can't add more flavours to the ice-cream (Stack is Full).\n");
  else
- printf("'%s' flavour has been added to your ice-cream 
-(Pushed in Stack).\n\n", nm);
+ printf("'%s' flavour has been added to your ice-cream (Pushed in Stack).\n\n", nm);
  break;
  case 2: // pop
  reply = pop(stack, &top, nm);
  if (replyst == -1)
- printf("\n No flavours has been added to the ice-cream 
-(Stack is Empty) \n");
+ printf("\n No flavours has been added to the ice-cream (Stack is Empty) \n");
  else
- printf("Removed (Popped) flavour of the ice-cream is : 
-%s\n", nm);
+ printf("Removed (Popped) flavour of the ice-cream is : %s\n", nm);
  printf("\n");
  break;
  case 3: //display
  display(stack, &top, nm);
  break;
  case 4:
- printf("Kindly make your payment...Hope you enjoyed the 
-ice-cream, please visit us again!!!:)");
+ printf("Kindly make your payment...Hope you enjoyed the ice-cream, please visit us again!!!:)");
  exit(0);
  default:
  printf("Invalid operation \n");
  }
- 7
+ 
  }
  break;
  }
